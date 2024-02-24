48ccfb542ac615b13bf653be4398a705714e959d
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2024-02-24 10:49:06 -0500

    Succsesfully generated a grass plane, Perlin noise generation needs tweaking

--- GMD200-GridGame/Assets/Scripts/TileManager.cs
+++ GMD200-GridGame/Assets/Scripts/TileManager.cs
@@ -14,8 +14,9 @@ public class TileManager : MonoBehaviour
 
     [SerializeField] private GameObject[] tileArray;
 
+    [SerializeField] private GameObject parentObject;
 
-    private int[,] currentGrid;
+    private GameObject[,] currentGrid;
 
     Dictionary<int, GameObject> tileset;
 
@@ -30,6 +31,7 @@ public class TileManager : MonoBehaviour
     /// </summary>
     void GenerateDictionary()
     {
+        tileset = new Dictionary<int, GameObject>();
         //Add Tiles to dictionary
         for (int i = 0; i < tileArray.Length; i++)
             tileset.Add(i, tileArray[i]);
@@ -38,20 +40,44 @@ public class TileManager : MonoBehaviour
     void GenerateTerrain()
     {
         //Initialize grid
-        currentGrid = new int[gridWidth, gridHeight];
+        currentGrid = new GameObject[gridWidth, gridHeight];
 
         for (int x = 0; x < gridWidth; x++)
         {
             for (int y = 0; y < gridHeight; y++)
             {
-                //currentGrid[x, y] = GeneratePerlinNoise(x, y);
+                int tileID = GeneratePerlinNoise(x, y);
+
+                GenerateTile(tileID, x, y);
             }
         }
     }
 
-   /* int GeneratePerlinNoise(int x, int y)
+    int GeneratePerlinNoise(int x, int y)
     {
         float rawPerlin = Mathf.PerlinNoise(x, y);
 
-    }*/
+        float clampedPerlin = Mathf.Clamp01(rawPerlin);
+
+        float scaledPerlin = clampedPerlin * tileset.Count;
+        
+        //If number out of bounds comes up, set it to highest value in dictionary
+        if (scaledPerlin == tileset.Count)
+            scaledPerlin = tileset.Count - 1;
+
+        return Mathf.FloorToInt(scaledPerlin);
+    }
+
+    void GenerateTile(int tileID, int x, int y)
+    {
+        GameObject tilePrefab = tileset[tileID];
+
+        GameObject tile = Instantiate(tilePrefab, parentObject.transform);
+        tile.transform.localPosition = new Vector3(x, y, 0);
+
+        tile.transform.parent = parentObject.transform;
+
+
+        currentGrid[x, y] = tile;
+    }
 }
