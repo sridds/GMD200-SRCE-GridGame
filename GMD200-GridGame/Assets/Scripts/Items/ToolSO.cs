07aff4eb991483741c89dd8d94b7564976638352
sridds <seth.riddensdale@mymail.champlain.edu>
2024-02-25 12:20:48 -0500

    Organized Item Scriptables
    Added respective folders for each scriptable object

--- GMD200-GridGame/Assets/Scripts/Items/ToolSO.cs
+++ GMD200-GridGame/Assets/Scripts/Items/ToolSO.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 [CreateAssetMenu(fileName = "Tool_SO", menuName = "Items/Tool", order = 1)]
 public class ToolSO : ItemSO
 {
-    
+    // In the future, this will be replaced with the corresponding class that this will break. Tiles will fill this list and be compared
+    public List<GameObject> Breakables;
 }
