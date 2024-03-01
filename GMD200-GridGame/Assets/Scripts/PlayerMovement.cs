d5b71d55538fedf61554b556164db30ef4e125ac
luke-murray <luke.murray@mymail.champlain.edu>
2024-03-01 17:15:38 -0500

    Changed player movement to tilebased somewhat. Player moves diagonally which is still in need of fixing.

--- GMD200-GridGame/Assets/Scripts/PlayerMovement.cs
+++ GMD200-GridGame/Assets/Scripts/PlayerMovement.cs
@@ -19,12 +19,19 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+
         float xInput = Input.GetAxisRaw("Horizontal");
         float yInput = Input.GetAxisRaw("Vertical");
 
         int newPosX = Mathf.FloorToInt(transform.position.x + xInput);
         int newPosY = Mathf.FloorToInt(transform.position.y + yInput);
 
+        Debug.Log(PerlinData.Instance.tiles[newPosX, newPosY].tileType);
+        Debug.Log(newPosX);
+        Debug.Log(newPosY);
+
+        if (!PerlinData.Instance.InBounds(newPosX, newPosY))
+            return;
         if (PerlinData.Instance.tiles[newPosX, newPosY].tileType == TileType.Water)
             return;
 
@@ -34,10 +41,13 @@ public class PlayerMovement : MonoBehaviour
         {
             if (Mathf.Abs(xInput) == 1f)
             {
-                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
-            }else if (Mathf.Abs(yInput) == 1f)
+                //movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                movePoint.position = PerlinData.Instance.tiles[newPosX, newPosY].tilePosition;
+            }
+            else if (Mathf.Abs(yInput) == 1f)
             {
-                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                //movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                movePoint.position = PerlinData.Instance.tiles[newPosX, newPosY].tilePosition;
             }
             
         }
