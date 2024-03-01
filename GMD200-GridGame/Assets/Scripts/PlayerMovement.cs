992836ff659e8d987575217736a47ce7e53aea51
luke-murray <luke.murray@mymail.champlain.edu>
2024-03-01 16:55:43 -0500

    Added fixed water check with perlin data

--- GMD200-GridGame/Assets/Scripts/PlayerMovement.cs
+++ GMD200-GridGame/Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -9,6 +10,8 @@ public class PlayerMovement : MonoBehaviour
 
     public LayerMask stopsMovement;
 
+    private Vector2 currentPosition;
+
     private void Start()
     {
         movePoint.parent = null;
@@ -16,22 +19,25 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        float xInput = Input.GetAxisRaw("Horizontal");
+        float yInput = Input.GetAxisRaw("Vertical");
+
+        int newPosX = Mathf.FloorToInt(transform.position.x + xInput);
+        int newPosY = Mathf.FloorToInt(transform.position.y + yInput);
+
+        if (PerlinData.Instance.tiles[newPosX, newPosY].tileType == TileType.Water)
+            return;
+
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed* Time.deltaTime);
 
         if(Vector3.Distance(transform.position, movePoint.position) <= 0.05)
         {
-            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            if (Mathf.Abs(xInput) == 1f)
             {
-                if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), 0.2f, stopsMovement))
-                {
                     movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
-                }
-            }else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            }else if (Mathf.Abs(yInput) == 1f)
             {
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), 0.2f, stopsMovement))
-                {
                     movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
-                }
             }
             
         }
