31190c7e811e019bd610509c35784f59e20c1812
sridds <seth.riddensdale@mymail.champlain.edu>
2024-03-04 21:13:51 -0500

    Inventory Revision
    The old inventory was causing major problems when scaling and also used a list as opposed to a grid based approach. Currently working on a grid-based approach with guidance from various tutorials.

---
+++ GMD200-GridGame/Assets/Scripts/Inventory/GenericGrid.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Thanks to code monkey for this tutorial
+/// </summary>
+public class GenericGrid<TGridObject>
+{
+    public event EventHandler<OnGridObjectChangedArgs> OnGridObjectChanged;
+    public class OnGridObjectChangedArgs : EventArgs
+    {
+        public int x;
+        public int y;
+    }
+
+    private int width;
+    private int height;
+    private TGridObject[,] gridArray;
+
+    public GenericGrid(int width, int height, Func<GenericGrid<TGridObject>, int, int, TGridObject> createGridObject)
+    {
+        this.width = width;
+        this.height = height;
+
+        gridArray = new TGridObject[width, height];
+
+        // initialize
+        for(int x = 0; x < gridArray.GetLength(0); x++) {
+            for(int y = 0; y < gridArray.GetLength(1); y++) {
+                gridArray[x, y] = createGridObject(this, x, y);
+            }
+        }
+    }
+
+    public void SetGridObject(int x, int y, TGridObject value) {
+        if (!IsIndexValid(x, y)) return;
+
+        gridArray[x, y] = value;
+    }
+
+    /// <summary>
+    /// Attempts to retrieve the value at the provided index
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <returns></returns>
+    public TGridObject GetGridObject(int x, int y)
+    {
+        if (!IsIndexValid(x, y)) return default(TGridObject);
+
+        return gridArray[x, y];
+    }
+
+    public void TriggerGridObjectChanged(int x, int y)
+    {
+        if (!IsIndexValid(x, y)) return;
+
+        if (OnGridObjectChanged != null) OnGridObjectChanged(this, new OnGridObjectChangedArgs { x = x, y = y });
+    }
+
+    /// <summary>
+    /// Validates an index, ensuring it does not go out of bounds of the array
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <returns></returns>
+    private bool IsIndexValid(int x, int y) => x < width && x >= 0 && y < height && y >= 0;
+}
---
+++ GMD200-GridGame/Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,153 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2Int dimensions;
+
+    private GenericGrid<Slot> slots;
+
+    private void Start()
+    {
+        // initialize
+        slots = new GenericGrid<Slot>(dimensions.x, dimensions.y, (GenericGrid<Slot> g, int x, int y) => new Slot(g, x ,y)); 
+    }
+
+    public bool AddItem(ItemSO item)
+    {
+        // search for empty item
+        if (item.Stack.CanStack)
+        {
+            for (int x = 0; x < dimensions.x; x++)
+            {
+                for (int y = 0; y < dimensions.y; y++)
+                {
+                    Slot slot = slots.GetGridObject(x, y);
+
+                    // null? set item
+                    if (slot.Item == null) continue;
+
+                    // try add to stack
+                    else if (slot.Item.ItemName == item.ItemName && slot.AddToStack()) return true;
+                }
+            }
+        }
+
+        // attempt to add item anywhere
+        if (TryAddItem(item)) return true;
+
+        return false;
+    }
+
+    public bool AddItemAtPosition(ItemSO item, int x, int y)
+    {
+        Slot slot = slots.GetGridObject(x, y);
+
+        // attempt to add item at empty slot
+        if(slot == null) {
+            slot.SetItem(item);
+            return true;
+        }
+        // stack item as long as they match
+        else if (slot.Item.ItemName == item.ItemName && slot.AddToStack()) return true;
+
+        // failed to add item at position
+        return false;
+    }
+
+    private bool TryAddItem(ItemSO item)
+    {
+        for (int x = 0; x < dimensions.x; x++)
+        {
+            for (int y = 0; y < dimensions.y; y++)
+            {
+                Slot slot = slots.GetGridObject(x, y);
+
+                if(slot.Item == null) {
+                    slot.SetItem(item);
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Swaps two items at the provided indexes
+    /// </summary>
+    /// <param name="i"></param>
+    /// <param name="j"></param>
+    public void SwapItems(Vector2Int i, Vector2Int j)
+    {
+        // get corresponding slots
+        Slot s1 = slots.GetGridObject(i.x, i.y);    
+        Slot s2 = slots.GetGridObject(j.x, j.y);
+
+        // ensure both exist
+        if (s1 == null || s2 == null) return;
+        Slot temp = s1;
+
+        // perform swap
+        s1.SetItem(s2.Item, s2.Stack);
+        s2.SetItem(temp.Item, temp.Stack);
+    }
+}
+
+public class Slot
+{
+    // grid references
+    private GenericGrid<Slot> grid;
+    private int x, y;
+
+    // item variables
+    private ItemSO item;
+    private int stack;
+
+    // accessors
+    public int Stack { get { return stack; } }
+    public ItemSO Item { get { return item; } }
+    public int MaxStack { get { return item.Stack.CanStack ? item.Stack.MaxStack : 1; } }
+
+    public Slot(GenericGrid<Slot> grid, int x, int y)
+    {
+        // set values
+        this.grid = grid;
+        this.x = x;
+        this.y = y;
+        stack = 0;
+
+        grid.TriggerGridObjectChanged(x, y);
+    }
+
+    public void SetItem(ItemSO item, int stack = 1)
+    {
+        this.item = item.Clone();
+        this.stack = stack;
+
+        grid.TriggerGridObjectChanged(x, y);
+    }
+
+    public bool AddToStack(int amount = 1)
+    {
+        if (stack >= MaxStack) return false;
+
+        stack += amount;
+        grid.TriggerGridObjectChanged(x, y);
+
+        return true;
+    }
+
+    public void RemoveFromStack(int amount = 1)
+    {
+        stack -= amount;
+
+        if(stack == 0) {
+            item = null;
+        }
+
+        grid.TriggerGridObjectChanged(x, y);
+    }
+}
